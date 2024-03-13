9111c3df0dfaa8eba9429fb92ae0503a53b46254
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:53:42 +0500

    first commit

---
+++ T-Shirt.js
@@ -0,0 +1,11 @@
+"use strict";
+// T-Shirt: Write a function called make_shirt()
+// that accepts a size and the text of a message that should be printed on the shirt.
+// The function should print a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function.
+function make_shirt(size, text) {
+    console.log(`creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'imran Khan Zindabad');
+make_shirt('medium', 'imran Khan Zindabad');
+make_shirt('small', 'imran Khan Zindabad');
